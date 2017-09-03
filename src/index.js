8a5fa5ab68bc3e340b840f7a17d2103772a41ec5
IvanovEd <social888>
2017-09-03 23:06:22 +0300

    implementing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_data';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyD-zCqHyAt-_awoEp6qaOysr62lfHPs4Is';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.onSearchVideo('ed')
+    }
+
+    onSearchVideo(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos : videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearchTimeLimit = _.debounce(term => {this.onSearchVideo(term)}, 300);
+        return (<dev>
+            <SearchBar onSearchVideo={videoSearchTimeLimit}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+                onVideoSelected={selectedVideo => this.setState({selectedVideo: selectedVideo})}
+                videos={this.state.videos}
+            />
+        </dev>);
+    }
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
